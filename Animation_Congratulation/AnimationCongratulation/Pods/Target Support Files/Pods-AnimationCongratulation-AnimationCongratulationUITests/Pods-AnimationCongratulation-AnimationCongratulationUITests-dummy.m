5f3debca664d8b94eeddbe193c29605ee9830dbd
DucCanh <canhgithub@gmail.com>
2020-06-24 23:28:00 -0700

    new project

--- Animation_Congratulation/AnimationCongratulation/Pods/Target Support Files/Pods-AnimationCongratulation-AnimationCongratulationUITests/Pods-AnimationCongratulation-AnimationCongratulationUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AnimationCongratulation_AnimationCongratulationUITests : NSObject
-@end
-@implementation PodsDummy_Pods_AnimationCongratulation_AnimationCongratulationUITests
-@end
